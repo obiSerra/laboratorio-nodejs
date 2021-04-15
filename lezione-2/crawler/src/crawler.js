2e60073f90bd3aa7224064fe97ab0d91f4a04217
Roberto Serra <roberto.serra@etiqa.it>
2021-04-15 17:33:17 +0200

    validazione url

--- lezione-2/crawler/src/crawler.js
+++ lezione-2/crawler/src/crawler.js
@@ -3,10 +3,12 @@
 const https = require("https");
 const { url } = require("inspector");
 var HTMLParser = require("node-html-parser");
+const { isRegExp } = require("util");
 
 class Crawler {
   constructor(startingUrl) {
     this.startingUrl = startingUrl;
+    this.visited = [];
   }
 
   findAnc(content) {
@@ -16,26 +18,45 @@ class Crawler {
     return urls;
   }
 
-  getPage(url, onEnd) {
-    https
-      .get(url, response => {
-        console.log("statusCode:", response.statusCode);
-        let data = "";
-        response.on("data", d => {
-          data += d;
-        });
-        response.on("end", () => {
-          if (typeof onEnd === "function") {
-            const urls = this.findAnc(data);
-            onEnd(urls);
-          }
-        });
-      })
-      .on("error", err => console.log(`[ERROR] ${err}`));
+  isValidUrl(u) {
+    try {
+      let url = null;
+      if (u.match(/^\//)) {
+        url = new URL(u, this.startingUrl.origin);
+      } else {
+        url = new URL(u);
+      }
+
+      return url.host === this.startingUrl.host;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  getPage(url) {
+    return new Promise((resolve, reject) => {
+      https
+        .get(url, response => {
+          console.log("statusCode:", response.statusCode);
+          this.visited.push(url.href);
+          console.log(this.visited);
+          let data = "";
+          response.on("data", d => {
+            data += d;
+          });
+          response.on("end", () => {
+            const urls = this.findAnc(data).filter(u => this.isValidUrl(u));
+            resolve(urls);
+          });
+        })
+        .on("error", err => reject(err));
+    });
   }
 
   run(onEnd) {
-    this.getPage(this.startingUrl, onEnd);
+    this.getPage(this.startingUrl).then(urls => {
+      console.log(urls);
+    });
   }
 }
 
